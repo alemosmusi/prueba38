b046d8ee02f3fc11f4fe428fd44e332384598bd1
Agu <alemosmusi@gmail.com>
2023-08-10 13:33:25 -0300

    agregado iconos

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,13 +1,25 @@
 import React, { useEffect } from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
 import { Text, View } from 'react-native'
+import { colores, styles } from '../theme/appTheme'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 
 const Tab1Screen = () => {
 
 
+  const {top}= useSafeAreaInsets()
+
+
 
   return (
-    <View>
-        <Text>Tab1Screen</Text>
+    <View style={{
+       ...styles.globalMargin,
+       marginTop: top +20
+       }
+       }>
+        <Text style={styles.title}>Tab1Screen</Text>
+        <Icon name="airplane-outline" size={50} color={colores.primary}/>
     </View>
   )
 }
